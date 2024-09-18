f028ae15c0e680919cd96e5ed14663539021e26a
JD Tadlock <jt7903@gmail.com>
2024-09-18 11:55:32 -0400

    finished conditional overview

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 const firstName = 'JD';
 let age = 44;
 
-// age = age + 5;
-// age += 5;
+// If age is greater than 20 and less than 50, console log 'you are young'
+if (age > 20 && age < 50) {
+  console.log('you are young');
+} else if (age >= 50) {
+  console.log('you are getting older');
+} else if (age < 20 && age > 12) {
+  console.log('you are a teenager');
+} else {
+  console.log('your age is not within our range');
+}
 
-// age = age - 5;
-// age -= 5;
+const result = (0 || 75) && true && 'some string' && true && 100;
 
-// age *= 5;
+console.log(result);
 
-// age = age + 1;
-age++;
-age++;
-age++;
+// If age is greater than or equal to 50, console log 'you are getting older'
 
-age--;
 
-console.log(age);
+// If age is less 20 and greater than 12, console log 'you are a teenager'
