eb555db269af308c72156c0c54dc22e6be5f443b
JD Tadlock <jt7903@gmail.com>
2024-09-18 11:16:33 -0400

    finished assignment operators

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const firstName = 'JD';
+let age = 44;
+
+// age = age + 5;
+// age += 5;
+
+// age = age - 5;
+// age -= 5;
+
+// age *= 5;
+
+// age = age + 1;
+age++;
+age++;
+age++;
+
+age--;
+
+console.log(age);
