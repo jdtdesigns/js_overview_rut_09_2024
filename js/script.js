b0a61bd4270536f2c8c95fd6ecfd2a26e1c91f3d
JD Tadlock <jt7903@gmail.com>
2024-09-18 13:03:39 -0400

    finished array basics

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Arrays are not primitive value types
+// They are reference types
+const fruits = ['apple', 'orange', 'kiwi', 'grape'];
+const names = ['Roman', 'Michael', 'Nina', 'Adonis'];
+const data = ['JD', 'Tadlock', 44, '555 coding dr', '777-777-7777', ['pickleball', 'fishing']];
+
+fruits[2] = 'strawberry';
+
+// Increase the data age number by one - console.log the original value and the new value after you increase it
+console.log(data[2]);
+
+data[2]++;
+
+console.log(data[2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
