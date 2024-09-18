2eef11c8b78d7aaf292a707456a022eb5675ed55
JD Tadlock <jt7903@gmail.com>
2024-09-18 14:49:34 -0400

    function overview started

--- js/script.js
+++ js/script.js
@@ -1,46 +1,17 @@
-// Arrays are not primitive value types
-// They are reference types
-const fruits = [
-  "Apple", "Banana", "Cherry", "Date", "Elderberry",
-  "Fig", "Grape", "Honeydew", "Kiwi", "Lemon",
-  "Mango", "Nectarine", "Orange", "Papaya", "Quince",
-  "Raspberry", "Strawberry", "Tangerine", "Ugli fruit", "Vanilla bean",
-  "Watermelon", "Xigua", "Yellow passion fruit", "Zucchini", "Apricot",
-  "Blackberry", "Cantaloupe", "Dragonfruit", "Elderflower", "Jackfruit",
-  "Kumquat", "Lychee", "Mulberry", "Nutmeg", "Olive",
-  "Peach", "Pineapple", "Pomegranate", "Starfruit", "Tamarind"
-];
-const names = ['Roman', 'Michael', 'Nina', 'Adonis'];
-const data = ['JD', 'Tadlock', 44, '555 coding dr', '777-777-7777', ['pickleball', 'fishing']];
+const names = ['Izzy', 'Matt', 'City', 'Kandyce'];
+const fruit = 'apple';
 
-const fullName = 'JD Tadlock';
-
-for (let index = 0; index < fullName.length; index++) {
-  console.log(fullName[index]);
+function printNames() {
+  const someVal = 'this is a val';
+  // Loop over the names array and print out each name to the console
+  for (let index = 0; index < names.length; index++) {
+    console.log(names[index]);
+  }
 }
 
-// for (let count = 1; count <= 10; count++) {
-//   console.log(count);
-// }
+printNames();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (true) {
+  const nestedVar = 'some nested var';
+  console.log(nestedVar);
+}
