04ed207e6d79da57508b122a08efd3d162ebf8fd
JD Tadlock <jt7903@gmail.com>
2024-09-19 12:38:36 -0400

    finished function basics

--- js/script.js
+++ js/script.js
@@ -9,9 +9,19 @@ function printNames() {
   }
 }
 
-printNames();
-
-if (true) {
-  const nestedVar = 'some nested var';
-  console.log(nestedVar);
+function add(num1, num2) {
+  return num1 + num2;
 }
+
+const result = add(3, 10);
+
+console.log(result);
+// add(10, 5);
+// add(20, 100);
+// add(50, 5);
+
+// function printVals(str, bool, num, arr) {
+//   console.log(arr[1]);
+// }
+
+// printVals('some string', true, 15, ['Michael', 'Nina', 'Roman']);
