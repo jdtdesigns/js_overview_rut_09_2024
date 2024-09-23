e7b962b325493a273660ea3f92245e76144805e0
JD Tadlock <jt7903@gmail.com>
2024-09-23 11:41:48 -0400

    finished timer app

--- js/script.js
+++ js/script.js
@@ -1,57 +1,50 @@
-const header = document.querySelector('h1');
-const paragraph = document.querySelector('p:nth-child(2)');
-const image = document.querySelector('#main-image');
-const noteOutput = document.querySelector('.output');
 const timerDisplay = document.querySelector('#time');
 const timerBtn = document.querySelector('#start-btn');
+const resetBtn = document.querySelector('#reset-btn');
 
-
-header.classList.add('crazy');
-
-
-header.style.textDecoration = 'underline';
-
-// image.setAttribute('src', 'https://images.pexels.com/photos/16112572/pexels-photo-16112572/free-photo-of-view-of-sun-shining-between-the-trees-in-a-park.jpeg');
-
-// Insert a new article element into the output section
-noteOutput.insertAdjacentHTML('beforeend', `
-  <article>
-    <h3>New note text</h3>
-    <p>Added On: 9/20/2024</p>
-  </article>
-`);
-
-// noteOutput.innerHTML = `
-//   <article>
-//     <h3>New note text</h3>
-//     <p>Added On: 9/20/2024</p>
-//   </article>
-// `;
-
-// Timers
-// setTimeout(function () {
-//   console.log('time up!');
-// }, 10000);
 let count = 10;
 let started = false;
+let timer;
 
+function resetTimer(eventObj) {
+  clearInterval(timer);
+  timerDisplay.innerText = 'Time: 10';
+  count = 10;
+  started = false;
+}
 
-timerBtn.addEventListener('click', function () {
+function startTimer(eventObj) {
   if (!started) {
-    const timer = setInterval(function () {
+    timer = setInterval(function () {
       count--;
 
       timerDisplay.innerText = 'Time: ' + count;
 
       if (count <= 0) {
         clearInterval(timer);
-
-        timerDisplay.innerText = 'Time: 10';
-        count = 10;
-        started = false;
       }
     }, 1000);
 
     started = true;
   }
-});
+}
+
+
+function getKeyPress(eventObject) {
+  // If the keyCode is 83, run startTimer()
+  if (eventObject.keyCode === 83) {
+    startTimer();
+  }
+
+  // If the keyCode is ?, run resetTimer()
+  if (eventObject.keyCode === 82) {
+    resetTimer();
+  }
+}
+
+
+timerBtn.addEventListener('click', startTimer);
+
+resetBtn.addEventListener('click', resetTimer);
+
+document.addEventListener('keydown', getKeyPress);
